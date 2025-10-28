7f13fb520ab1192684838205b58091ff1d8b83a0
X <den2007fad@gmail.com>
2025-10-28 13:02:58 +0300

    feat(db): Описаны модели для Account и Token

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,17 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { IS_DEV_ENV } from './libs/common/utils/is-dev.utik';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { IS_DEV_ENV } from './libs/common/utils/is-dev.utik'
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      ignoreEnvFile: !IS_DEV_ENV,
-      isGlobal: true
-    })
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			ignoreEnvFile: !IS_DEV_ENV,
+			isGlobal: true
+		})
+	],
+	controllers: [AppController],
+	providers: [AppService]
 })
 export class AppModule {}
--- server/src/libs/common/utils/is-dev.utik.ts
+++ server/src/libs/common/utils/is-dev.utik.ts
@@ -1,12 +1,10 @@
-import { ConfigService } from "@nestjs/config";
 import * as dotenv from 'dotenv'
 
+import { ConfigService } from '@nestjs/config'
 
 dotenv.config()
 
+export const isDev = (ConfigService: ConfigService) =>
+	ConfigService.getOrThrow('NODE_ENV') === 'development'
 
-export const isDev = (ConfigService: ConfigService) => 
-    ConfigService.getOrThrow('NODE_ENV') === 'development'
-
-
-export const IS_DEV_ENV = process.env.NODE_ENV  === 'development'
+export const IS_DEV_ENV = process.env.NODE_ENV === 'development'
